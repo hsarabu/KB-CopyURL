3eb177665fe28bfb37b2dd38145818dc9393e870
Hitesh <hsarabu@wisc.edu>
2016-04-18 17:50:05 -0500

    Moving JS into inject.js so that we can read whats on the DOM. Fixed it so that we get the correct external KB doc at the end.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,9 +5,35 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		changePage();
 		// ----------------------------------------------------------
 
 	}
 	}, 10);
 });
+var url;
+
+function changePage(){
+	if(hasExternalPage() != -1){ //check to see if an external page exists
+		var finalPage = "http://kb.wisc.edu/page.php?id=" + getKbId(); //append the doc ID to the standard kb url Base
+		var element = document.createElement("P");
+		var text = document.createTextNode("An external site exists, click anywhere in the banner to copy me onto the clipboard" + finalPage);
+		element.appendChild(text);
+		document.getElementById("logo-search").appendChild(element);
+	}
+};
+
+
+function getKbId() {
+	url = window.location + ""; //grab current URL to get the KB doc number
+	var tempList = url.split("=");
+	return tempList[1];
+
+};
+
+function hasExternalPage(){
+
+	var HTML = document.getElementsByTagName('html')[0].innerHTML;
+	return HTML.search("-external");
+	};
+
